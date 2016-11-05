a9a2203fc41f615037a4ccc33691317ef5b803ea
Garcia Florian <garcia.floriian@gmail.com>
2016-11-05 23:09:17 +0100

    Skeleton java app

---
+++ java-based/server/src/main/java/com/ippon/microservices/ServerApplication.java
@@ -0,0 +1,14 @@
+package com.ippon.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
