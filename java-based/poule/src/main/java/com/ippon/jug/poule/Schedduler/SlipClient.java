ac4fd750c2b2e76aaf64164dfea001257974f544
Garcia Florian <garcia.floriian@gmail.com>
2016-11-14 08:16:36 +0100

    Add fallback method for readability on SlipClient

--- java-based/poule/src/main/java/com/ippon/jug/poule/Schedduler/SlipClient.java
+++ java-based/poule/src/main/java/com/ippon/jug/poule/Schedduler/SlipClient.java
@@ -1,12 +1,25 @@
 package com.ippon.jug.poule.Schedduler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "slip")
+@FeignClient(value = "slip", fallback = SlipClientFallback.class)
 interface SlipClient {
 
     @RequestMapping(value = "/request", method = RequestMethod.GET)
     String getRequest();
+}
+
+class SlipClientFallback implements SlipClient {
+
+    private Logger logger = LoggerFactory.getLogger(SlipClient.class);
+
+    @Override
+    public String getRequest() {
+        logger.error("Could not reach slip");
+        return null;
+    }
 }
