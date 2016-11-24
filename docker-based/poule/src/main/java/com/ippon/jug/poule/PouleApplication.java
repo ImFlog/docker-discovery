1eb76225148313fe0874d0c891be6f938e1fc558
Garcia Florian <garcia.florian.perso@gmail.com>
2016-11-24 14:00:33 +0100

    Disable inifnite cache

--- docker-based/poule/src/main/java/com/ippon/jug/poule/PouleApplication.java
+++ docker-based/poule/src/main/java/com/ippon/jug/poule/PouleApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 public class PouleApplication {
 
     public static void main(String[] args) {
+        System.setProperty("networkaddress.cache.ttl", "5");
         SpringApplication.run(PouleApplication.class, args);
     }
 }
